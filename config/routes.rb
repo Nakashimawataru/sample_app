fecba933a2ffd6fc99edecc6d9997aa69b81ab89
EC2 Default User <130224869+Nakashimawataru@users.noreply.github.com>
2023-04-14 09:25:59 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'lists/new'
+  post 'lists'=>'lists#create'
+  get  'lists'=>'lists#index'
+  get 'lists/edit'
+  get '/top'=>'homes#top'
+  get 'lists/:id' => 'lists#show'
+end
